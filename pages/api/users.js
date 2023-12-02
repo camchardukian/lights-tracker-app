a1e35851c5218f8fd66280372818a73f807f1e92
Cameron Chardukian <camchardukian@gmail.com>
2023-12-02 19:29:47 +0700

    prevent accounts with duplicate emails from being registered

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,10 +1,31 @@
 import bcrypt from "bcryptjs";
 import { connectToDatabase } from "../../db";
 
+async function checkIfUserExists(email) {
+  try {
+    const db = await connectToDatabase();
+    const usersCollection = db.collection("users");
+
+    const existingUser = await usersCollection.findOne({ email });
+
+    return !!existingUser;
+  } catch (error) {
+    console.error("Error checking if user exists:", error);
+    throw error;
+  }
+}
+
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, email, password } = req.body;
     try {
+      const userExists = await checkIfUserExists(email);
+
+      if (userExists) {
+        return res
+          .status(400)
+          .json({ error: "User with this email already exists" });
+      }
       const newUser = await createUser(name, email, password);
       res.status(201).json(newUser);
     } catch (error) {
@@ -40,11 +61,3 @@ export async function createUser(name, email, password) {
     throw error;
   }
 }
-
-// async function checkIfUserExists(email) {
-//   try {
-
-//   } catch (error) {
-
-//   }
-// }
